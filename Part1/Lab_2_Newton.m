37132ad2c248a26b18f6e58be249c3714b9c83d3
khail13 <70116746+khail13@users.noreply.github.com>
2022-03-07 15:07:16 -0800

    Add files via upload

---
+++ Part1/Lab_2_Newton.m
@@ -0,0 +1,17 @@
+function [X,traj,f,k,Err] = Lab_2_Newton(x0,tol)
+    k = 0; ea = 1;
+    X = x0; traj=[];
+    f = Lab_2_Fun(X);
+    Err=NaN;
+    while ea > tol
+        grad = Lab_2_Grad(X);
+        hess = Lab_2_Hess(X);
+        xnew = X - inv(hess)*grad;
+        traj = [traj xnew]; %Store solution in a vector
+        k = k+1;
+        ea = norm(xnew-X); %Evaluate error
+        Err = [Err ea];
+        X = xnew;
+        f = Lab_2_Fun(X);
+    end
+end
---
+++ Part1/Lab_2_main.m
@@ -0,0 +1,29 @@
+clear;
+clc;
+
+%Define call function numerically
+[x1,y1] = meshgrid(-5:0.1:5,-5:0.1:5);
+f = (x1.^2 + y1 - 11).^2 + (x1 + y1.^2 - 7).^2;
+
+% Plot surface
+surface = figure; figure(surface);
+surf(x1,y1,f); shading interp;
+x0 = [0; 0]; %Initial Guess
+
+%Call Methods (Use % before each line to comment the methods you are not testing)
+
+%[X,traj,Z,k,Err] = Lab_2_sdm(x0, 10^(-4));
+%[X,traj,Z,k,Err] = Lab_2_Newton(x0, 10^(-4));
+% 
+%options = optimoptions('fminunc','GradObj','on','Algorithm','trust-region');
+%[X,FVAL,EXITFLAG,OUTPUT] = fminunc(@Lab_2_Fun,x0);
+
+
+
+% Add trajectory for finding minima (3D plot)
+%plot3(traj(:,1), traj(:,2), Z(:,1) ,'-k+')
+
+%Plot contour (2D plot)
+contour_graph = figure; figure(contour_graph);
+contour(x1,y1,f,100); hold on;
+plot(X(1,:), X(2,:),'-k+')
