9f1e99a155bee4539d9c024edee4f56befc1c51d
khail13 <70116746+khail13@users.noreply.github.com>
2022-04-06 22:33:51 -0700

    Add files via upload

--- MSE211Lab4.m
+++ MSE211Lab4.m
@@ -20,13 +20,16 @@ vMax3 = max(abs(data.speed3));
 vMax4 = max(abs(data.speed4));
 vMax5 = max(abs(data.speed5));
 
-vMax = [vMax1, vMax2, vMax3, vMax4, vMax5];
+vMax = [vMax1*L, vMax2*L, vMax3*L, vMax4*L, vMax5*L];
 
 p1 = polyfit(thetaMax(1:4), vMax(1:4), 1);
 vMax5_model = p1*[thetaMax5; 1];
-error = abs((vMax5 - vMax5_model)/vMax5);
+error = abs((vMax5*L - vMax5_model)/vMax5/L);
 
-%plot(0:75, p1(1)*(0:75) + p1(2), thetaMax(1:4), vMax(1:4), 'o', thetaMax5, vMax5_model, '*');
+% plot(0:75, p1(1)*(0:75) + p1(2), thetaMax(1:4), vMax(1:4), 'o', thetaMax5, vMax5_model, '*', thetaMax(5), vMax(5), 'o');
+% legend('Polyfit Model', 'Experimental Data', 'Predicted Linear Velocity of Trial 5' , 'Experimental Linear Velocity of Trial 5');
+% xlabel('Angle, \theta (\circ)');
+% ylabel('Linear Velocity, V (m/s)');
 
 t0 = 0;
 v0 = 0;
@@ -49,35 +52,39 @@ f = @(t,x,v) -(m*g*r*sin(x))/(I+m*r^2);
 
 filter(1, 1, -data.speed1);
 
-% figure();
-% plot(0:h:8, v1, data.time1, -data.speed1);
-% legend('Numerical Method', 'Experimental Data');
-% title('Pendulum Velocity'); subtitle('\theta_{0} = 8.47\circ');
-% xlabel('time, t (s)'); ylabel('velocity, V (m/s)');
-% 
-% figure();
-% plot(0:h:8, v2, data.time2, data.speed2);
-% legend('Numerical Method', 'Experimental Data');
-% title('Pendulum Velocity'); subtitle('\theta_{0} = 26.06\circ');
-% xlabel('time, t (s)'); ylabel('velocity, V (m/s)');
-% 
-% figure();
-% plot(0:h:8, v3, data.time3, data.speed3);
-% legend('Numerical Method', 'Experimental Data');
-% title('Pendulum Velocity'); subtitle('\theta_{0} = 38.34\circ');
-% xlabel('time, t (s)'); ylabel('velocity, V (m/s)');
-% 
-% figure();
-% plot(0:h:8, v4, data.time4, data.speed4);
-% legend('Numerical Method', 'Experimental Data');
-% title('Pendulum Velocity'); subtitle('\theta_{0} = 52.75\circ');
-% xlabel('time, t (s)'); ylabel('velocity, V (m/s)');
-% 
-% figure();
-% plot(0:h:8, v5, data.time5, data.speed5);
-% legend('Numerical Method', 'Experimental Data');
-% title('Pendulum Velocity'); subtitle('\theta_{0} = 64.17\circ');
-% xlabel('time, t (s)'); ylabel('velocity, V (m/s)');
+figure();
+plot(0:h:8, v1*L, data.time1, data.speed1*L);
+legend('Numerical Method', 'Experimental Data');
+title('Pendulum Velocity'); subtitle('\theta_{0} = 8.47\circ');
+xlabel('time, t (s)'); ylabel('velocity, V (m/s)');
 
+figure();
+plot(0:h:8, v2*L, data.time2, data.speed2*L);
+legend('Numerical Method', 'Experimental Data');
+title('Pendulum Velocity'); subtitle('\theta_{0} = 26.06\circ');
+xlabel('time, t (s)'); ylabel('velocity, V (m/s)');
 
+figure();
+plot(0:h:8, v3*L, data.time3, data.speed3*L);
+legend('Numerical Method', 'Experimental Data');
+title('Pendulum Velocity'); subtitle('\theta_{0} = 38.34\circ');
+xlabel('time, t (s)'); ylabel('velocity, V (m/s)');
 
+figure();
+plot(0:h:8, v4*L, data.time4, data.speed4*L);
+legend('Numerical Method', 'Experimental Data');
+title('Pendulum Velocity'); subtitle('\theta_{0} = 52.75\circ');
+xlabel('time, t (s)'); ylabel('velocity, V (m/s)');
+
+figure();
+plot(0:h:8, v5*L, data.time5, data.speed5*L);
+legend('Numerical Method', 'Experimental Data');
+title('Pendulum Velocity'); subtitle('\theta_{0} = 64.17\circ');
+xlabel('time, t (s)'); ylabel('velocity, V (m/s)');
+
+% 
+% figure();
+% plot(data.time1, data.speed1*L, data.time2, data.speed2*L, data.time3, data.speed3*L, data.time4, data.speed4*L, data.time5, data.speed5*L);
+% legend('Trial 1', 'Trial 2', 'Trial 3', 'Trial 4', 'Trial 5');
+% title('Experimental Data of Pendulum Velocity');
+% xlabel('time, t (s)'); ylabel('Linear Velocity, V (m/s)');
